9af92dede6748bdff6c18ca09430021214ea3a0c
maurbin <Diosmequiere0>
2021-03-12 20:32:47 +0100

    arreglado

--- app/src/main/java/com/example/android/materialme/DetailActivity.java
+++ app/src/main/java/com/example/android/materialme/DetailActivity.java
@@ -2,10 +2,6 @@ package com.example.android.materialme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
 
